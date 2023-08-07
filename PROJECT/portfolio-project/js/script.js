79d629c4407a981c22ce95fee08d8baecca379c2
Priyanshi-Vadadoriya <priyanshivadadoriya888@gmail.com>
2023-08-07 14:52:11 +0530

    .

--- PROJECT/portfolio-project/js/script.js
+++ PROJECT/portfolio-project/js/script.js
@@ -7,31 +7,31 @@ pro6 = document.getElementById('progress-bar-6');
 head = document.getElementById('head');
 
 
-        function test()
+    function test()
+    {
+        if(document.documentElement.scrollTop > 1000)
         {
-            if(document.documentElement.scrollTop > 900)
-            {
-                pro1.style.background = "conic-gradient(#7d2a7d 320deg, #ededed 0deg)";
-                pro2.style.background = "conic-gradient(#7d2a7d 300deg, #ededed 0deg)";
-                pro3.style.background = "conic-gradient(#7d2a7d 280deg, #ededed 0deg)";
-                pro4.style.background = "conic-gradient(#7d2a7d 260deg, #ededed 0deg)";
-                pro5.style.background = "conic-gradient(#7d2a7d 250deg, #ededed 0deg)";
-                pro6.style.background = "conic-gradient(#7d2a7d 180deg, #ededed 0deg)";
-                head.style.background = "#004773";
-
-            }
-            else
-            {
-                x.style.background = "conic-gradient(#7d2a7d 0deg, #ededed 0deg)";
-            }
+            pro1.style.background = "conic-gradient(#7d2a7d 320deg, #ededed 0deg)";
+            pro2.style.background = "conic-gradient(#7d2a7d 300deg, #ededed 0deg)";
+            pro3.style.background = "conic-gradient(#7d2a7d 280deg, #ededed 0deg)";
+            pro4.style.background = "conic-gradient(#7d2a7d 260deg, #ededed 0deg)";
+            pro5.style.background = "conic-gradient(#7d2a7d 250deg, #ededed 0deg)";
+            pro6.style.background = "conic-gradient(#7d2a7d 180deg, #ededed 0deg)";
+            head.style.background = "#004773";
         }
+        else
+        {
+            x.style.background = "conic-gradient(#7d2a7d 0deg, #ededed 0deg)";
+            head.style.background = "white";
+        }
+    }
 // side bar script
-        function openNav() {
-            document.getElementById("mySidebar").style.width = "200px";
-            document.getElementById("main").style.marginLeft = "200px";
-          }
+    function openNav() {
+        document.getElementById("mySidebar").style.width = "200px";
+        document.getElementById("main").style.marginLeft = "200px";
+    }
           
-          function closeNav() {
-            document.getElementById("mySidebar").style.width = "0";
-            document.getElementById("main").style.marginLeft= "0";
-          }
+    function closeNav() {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+    }
